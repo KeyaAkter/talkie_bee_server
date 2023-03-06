4072ec9d3b1742fa50493a4fb28b4fbd70cd6f3b
KeyaAkter <sanhatanvin12@gmail.com>
2023-03-06 23:45:50 +0600

    login controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const validator = require("validator");
+const mongoose = require("mongoose");
 
 // generate token
 const createToken = (_id) => {
@@ -10,9 +11,8 @@ const createToken = (_id) => {
 
 // registering user
 const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
-
     const existingUser = await userModel.findOne({ email });
 
     if (existingUser) {
@@ -54,6 +54,73 @@ const registerUser = async (req, res) => {
   }
 };
 
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = userModel.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json("Invalid email or password!");
+    }
+
+    // compairing password
+    const isValidPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isValidPassword) {
+      return res.status(400).json("Incorrect Password!");
+    }
+
+    // creating a new token
+    const token = createToken(existingUser._id);
+
+    res.status(200).json({
+      id: existingUser._id,
+      name: existingUser.name,
+      email,
+      password: existingUser.password,
+      token,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// finding an user
+const findUser = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json("Invalid Id!");
+  }
+
+  try {
+    const user = await userModel.findById(userId);
+
+    res.status(200).json(user);
+  } catch {
+    res.status(500).json(err);
+  }
+};
+
+// get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find({});
+
+    res.status(500).json(err);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  findUser,
+  getAllUsers,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  //findUser,
+  //getAllUser,
+} = require("../controllers/userController");
 
 // router
 const router = express.Router();
 
 //routes
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+//router.post("/find/:userId", findUser);
+//router.get("/", findUser);
 
 module.exports = router;
